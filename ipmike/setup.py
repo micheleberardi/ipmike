bd320cf7f2689427b17f9b3204e2fb33169e204c
Michele <micheleberardi@pm.me>
2023-02-14 17:25:32 -0500

    new version

--- ipmike/setup.py
+++ ipmike/setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
     name='ipmike',
-    version='0.1.3',
+    version='0.1.4',
     author='Your Name',
     author_email='your.email@example.com',
     url='https://github.com/micheleberardi/ipmike',
@@ -19,6 +22,6 @@ setup(
         'Programming Language :: Python :: 3.9',
     ],
     description= 'A package for geolocating IP addresses using the IPMike API',
-    long_description='A package for geolocating IP addresses using the IPMike API. The package provides a simple and easy-to-use interface for querying IP location data, and supports both IPv4 and IPv6 addresses. To use the package, simply create an instance of the IPMike class and call the get_location method with an IP address. The package requires an API token, which can be obtained by signing up for an account on the IPMike website. For more information, see the project README.',
-    long_description_content_type='text/markdown'
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
